b1368f0a437cf8e249ad73f6a2461a86fbf3a26b
Confidence Ndubuisi <samo188jiren@GOKU.com>
2025-10-25 12:30:11 +0100

    react project

--- src/calculator.js
+++ src/calculator.js
@@ -1,58 +1,63 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import './calc.css'
-import { Lexer } from "./lexer";
 
 function Inputarea({ name, value, onChangeValue }) {
-    return <div className={name} onChange={onChangeValue}>{value}</div>
+    return <div className={name}> {value} </div>
 }
-function OutputArea({name, value}) {
+function OutputArea({ name, value }) {
     return <div className={name}>{value}</div>
 }
 
-function Button({ NameClass, Bvalue, onButtonClick}) {
+function Button({ NameClass, Bvalue, onButtonClick }) {
     return <button className={NameClass} onClick={onButtonClick}>{Bvalue}</button>
 }
 
 
 
 function Calculator() {
-    const [TokenUsed, setTokenUsed] = useState([])
+    // const [numbre, setNumber] = useState();
     const [CurrentValue, setCurrentValue] = useState('');
-    const [evalAnswer, setevalAnswer ] = useState(0)
+    const [Output, setOutput] = useState(0)
+    const [NumberOne, setNumberOne] = useState(0);
+    const [Operator, setOperator] = useState('');
+    const [NumberTwo, setNumberTwo] = useState(0);
+    const prevValue = useRef(0);
 
-    function handleTokenUsed() {
-        let current = 0
+    useEffect(() => {
+      prevValue.current = CurrentValue;
+      if(/\d/.test(CurrentValue)) {
+        setOutput(prevValue.current);
+        console.log(Output[5]);
         
-        while (current < CurrentValue.length) {
-            alert("hi")
-        }
+      }
+        if (/[+\-*/]/.test(CurrentValue)) {
+              console.log(prevValue.current);  
+              setOutput(prevValue.current);
+              console.log(Output);
     }
+    
+}, [CurrentValue])
+    
 
     function handleClick(val) {
         setCurrentValue((prevNumber) => [...prevNumber, val]);
     }
 
     // var x = 78 + 87;
-
-    // function  evaluation
-    function evaluation() {
-        setevalAnswer(eval(CurrentValue));
-    }
-
     return (
         <>
             <center>
                 <section>
-                    <Inputarea name={"inputarea"} value={CurrentValue} onChangeValue={handleTokenUsed} />
-                    <OutputArea name={"inputarea output"} value={evalAnswer} />
-                    <div className="ButtonDiv">                        
+                    <Inputarea name={"inputarea"} value={CurrentValue} />
+                    <OutputArea name={"inputarea output"} value={Output} />
+                    <div className="ButtonDiv">
 
                         <Button NameClass={"Button"} Bvalue={7} onButtonClick={() => handleClick(7)} />
                         <Button NameClass={"Button"} Bvalue={8} onButtonClick={() => handleClick(8)} />
                         <Button NameClass={"Button"} Bvalue={9} onButtonClick={() => handleClick(9)} />
                         <Button NameClass={"Button"} Bvalue={"/"} onButtonClick={() => handleClick("/")} />
                         {/* <Button NameClass={"Button"} Bvalue={"pie"} onButtonClick={() => handleClick(22/7)} /> */}
-                        
+
 
                         <Button NameClass={"Button"} Bvalue={4} onButtonClick={() => handleClick(4)} />
                         <Button NameClass={"Button"} Bvalue={5} onButtonClick={() => handleClick(5)} />
@@ -64,10 +69,10 @@ function Calculator() {
                         <Button NameClass={"Button"} Bvalue={3} onButtonClick={() => handleClick(3)} />
                         <Button NameClass={"Button"} Bvalue={"-"} onButtonClick={() => handleClick("-")} />
 
-                        <Button NameClass={"Button"} Bvalue={"="} onButtonClick={() => evaluation()} />
+                        <Button NameClass={"Button"} Bvalue={"="} onButtonClick={() => {}} />
                         <Button NameClass={"Button"} Bvalue={0} onButtonClick={() => handleClick(0)} />
                         <Button NameClass={"Button"} Bvalue={"+"} onButtonClick={() => handleClick("+")} />
-                        <Button NameClass={"Button"} Bvalue={"pie"} onButtonClick={() => handleClick(22/7)} />
+                        <Button NameClass={"Button"} Bvalue={"pie"} onButtonClick={() => handleClick(22 / 7)} />
                     </div>
 
                 </section>
